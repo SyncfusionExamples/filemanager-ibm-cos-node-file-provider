03b638111b5058c9ea23270951682a5778416661
sivakumar <93644655+sivakumars3442@users.noreply.github.com>
2024-05-17 13:00:36 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,3 @@
-const gulp = require('gulp');
-const shell = require('gulp-shell');
-
-// Task to run 'npm install'
-gulp.task('install', shell.task('npm install'));
-
-// Task to run 'npm start'
-gulp.task('start', shell.task('npm start'));
-
-// Default task to run both 'install' and 'start' sequentially
-gulp.task('default', gulp.series('install', 'start'));
+var gulp = require("gulp");
+require('require-dir')('build');
+module.exports = gulp;
